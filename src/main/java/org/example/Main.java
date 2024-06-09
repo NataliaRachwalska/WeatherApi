a2fb4bbd31aa42997e5188ac04e4052b66a4e587
NataliaRachwalska <103137780+NataliaRachwalska@users.noreply.github.com>
2024-06-09 13:28:47 +0200

    Update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.swing.*;
+import java.awt.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    private JPanel mainPanel;
+    private JPanel panelContainer;
+    private CardLayout cardLayout;
+    private InfoPanel infoPanel;
+
+    public Main() {
+        cardLayout = new CardLayout();
+        panelContainer = new JPanel(cardLayout);
+
+        Weather weatherPanel = new Weather(this);
+        infoPanel = new InfoPanel();
+
+        panelContainer.add(weatherPanel.getMainPanel(), "WeatherPanel");
+        panelContainer.add(infoPanel.getMainPanel(), "InfoPanel");
+
+        mainPanel = new JPanel(new BorderLayout());
+        mainPanel.add(panelContainer, BorderLayout.CENTER);
+
+        cardLayout.show(panelContainer, "WeatherPanel");
+    }
+
+    public void showPanel(String panelName) {
+        cardLayout.show(panelContainer, panelName);
+    }
+
+    public void updateInfoPanel(String city, String condition,int pressure, int temperature, int feelsLike) {
+        infoPanel.getCitytextField().setText(city);
+        infoPanel.getConditiontextField().setText(condition);
+        infoPanel.getPressuretextField().setText(String.valueOf(pressure) + " hPa");
+        infoPanel.getTemperaturetextField().setText(String.valueOf(temperature) + " °C");
+        infoPanel.getPTemptextField().setText(String.valueOf(feelsLike) + " °C");
+        showPanel("InfoPanel");
+    }
+
+    public JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Weather Application");
+        frame.setContentPane(new Main().getMainPanel());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
     }
 }
