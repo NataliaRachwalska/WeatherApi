24151136588878d4e83f6950c950e22882da3ee6
NataliaRachwalska <103137780+NataliaRachwalska@users.noreply.github.com>
2024-06-09 13:28:13 +0200

    Update

--- src/main/java/org/example/InfoPanel.java
+++ src/main/java/org/example/InfoPanel.java
@@ -1,2 +1,36 @@
-package org.example;public class InfoPanel {
+package org.example;
+
+import javax.swing.*;
+
+public class InfoPanel {
+    private JPanel MainPanel;
+    private JTextField CitytextField;
+    private JTextField ConditiontextField;
+    private JTextField PressuretextField;
+    private JTextField TemperaturetextField;
+    private JTextField PTemptextField;
+
+    public JTextField getCitytextField() {
+        return CitytextField;
+    }
+
+    public JTextField getConditiontextField() {
+        return ConditiontextField;
+    }
+
+    public JTextField getPTemptextField() {
+        return PTemptextField;
+    }
+
+    public JTextField getPressuretextField() {
+        return PressuretextField;
+    }
+
+    public JTextField getTemperaturetextField() {
+        return TemperaturetextField;
+    }
+
+    public JPanel getMainPanel() {
+        return MainPanel;
+    }
 }
